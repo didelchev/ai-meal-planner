b7d7d4a3d070aa2993214fa9f4fb26d813ea9f77
Daniel Delchev <daniel.delchev@yahoo.com>
2026-02-24 18:42:59 +0200

    created authController

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,27 @@
+import { Request, Response, Router } from "express";
+import { authService } from "../services/authService";
+
+const authController = Router();
+
+authController.post('/register', async (req: Request, res: Response) => { 
+    const { email, username, password } = req.body;
+
+    try {
+        const user = await authService.register(email, username, password)
+        res.status(201).json(user)
+    } catch (error) {
+        if(error instanceof Error) {
+            res.status(400).json({message: error.message})
+        }else{
+            res.status(400).json({message: "An unknown error occured !"})
+        }
+    }
+
+})
+
+authController.post('/login', async (req: Request, res: Response) => { 
+    
+
+})
+
+export default authController
