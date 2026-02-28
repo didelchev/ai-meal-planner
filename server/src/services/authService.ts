c2e2fd5ee10fb0b01eb709a11c253f24ae58accf
Daniel Delchev <daniel.delchev@yahoo.com>
2026-02-28 16:09:08 +0200

    add login service

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,10 +1,19 @@
 import { createUser, findUserByEmail } from "../db/queries/userQueries";
-import { User } from "../types/user.types";
+import { AuthResponse, User } from "../types/user.types";
 import bcrypt from 'bcrypt';
+import { generateToken } from "../utils/generateToken";
+import { isValidEmail, isValidPassword } from "../utils/validators";
+
+
 
 
 export const authService = { 
-   async register(email: string, username: string, password: string):Promise<User> { 
+   async register(email: string, username: string, password: string): Promise<AuthResponse> { 
+
+        //TODO: Remove validations from authService
+        isValidEmail(email);
+
+        isValidPassword(password);
 
         const existingUser = await findUserByEmail(email);
 
@@ -12,14 +21,51 @@ export const authService = {
             throw new Error("User with that email already exists !")
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = await createUser(email, username, hashedPassword)
+        const user = await createUser(email, username, hashedPassword);
 
-        return user
+        const token = generateToken(user.id, user.email)
+
+         return { 
+            _id: user.id,
+            email: user.email,
+            username: user.username,
+            accessToken: token
+        }
     },
 
-    async login(email: string): Promise<void> {
+    async login(email: string, password: string): Promise<AuthResponse> {
+
+        //TODO: Remove validations from authService
+
+        isValidEmail(email);
+
+        isValidPassword(password);
+
+        const user = await findUserByEmail(email);
+
+        if(!user) { 
+            throw new Error("User with this email doesn't exist !");
+        }
+
+        const isPasswordMatching = await bcrypt.compare(password, user.password_hash);
+
+        if(!isPasswordMatching){
+            throw new Error("Wrong email address or password !");
+        }
+
+        const token = generateToken(user.id, user.email);
+
+        return { 
+            _id: user.id,
+            email: user.email,
+            username: user.username,
+            accessToken: token
+        }
+
+
+
 
     }
 }
