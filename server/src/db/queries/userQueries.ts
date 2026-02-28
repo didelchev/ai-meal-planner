877f84c4a01ddc5c4b95dcd5f1a7eb961c88e0c0
Daniel Delchev <daniel.delchev@yahoo.com>
2026-02-28 16:09:42 +0200

    updated createUser query to  return username from DB

--- server/src/db/queries/userQueries.ts
+++ server/src/db/queries/userQueries.ts
@@ -1,19 +1,20 @@
 import pool from "../pool";
-import { User } from "../../types/user.types";
+import { User, UserWithPassword } from "../../types/user.types";
 
 export const createUser = async (email: string, username: string, hashedPassword: string):Promise<User> => { 
 
     const result = await pool.query(`
         INSERT INTO users (email, username, password_hash)
         VALUES ($1, $2, $3) 
-        RETURNING id, email, created_at`, 
+        RETURNING id, email, username, created_at`, 
         [email, username, hashedPassword]);
 
+        
     return result.rows[0]
 }
 
 
-export const findUserByEmail = async (email: string): Promise<User> => { 
+export const findUserByEmail = async (email: string): Promise<UserWithPassword> => { 
     const result = await pool.query(
         `SELECT * FROM users WHERE email = $1`,
         [email]
