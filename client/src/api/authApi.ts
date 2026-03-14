c124c96a071e79b24dd6aec4942e61bd07654589
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-14 20:04:58 +0200

    update auth api

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -1,14 +1,18 @@
-import type { User } from "../types/user.types"
+import type { User, UserResponse } from "../types/user.types"
 import { API } from "../utils/fetcher"
 
 const AUTH_API_ENDPOINT = 'http://localhost:5000/auth'
 
 export const authAPI = { 
     async login(email: string, password: string): Promise<User>{
-        return await API.post(`${AUTH_API_ENDPOINT}/login`,{email, password} )
+        const response = await API.post<UserResponse>(`${AUTH_API_ENDPOINT}/login`,{email, password} )
+
+        return response.data
 
     },
     async register(email: string, username: string, password: string): Promise<User>{
-        return await API.post(`${AUTH_API_ENDPOINT}/register`,{email, username, password} )
+        const response =  await API.post<UserResponse>(`${AUTH_API_ENDPOINT}/register`,{email, username, password} )
+
+        return response.data    
     }
 }
