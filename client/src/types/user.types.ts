93b3310c95e10c767037634a854ad5fd47496295
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-14 20:07:02 +0200

    update AuthProvider

--- client/src/types/user.types.ts
+++ client/src/types/user.types.ts
@@ -1,15 +1,29 @@
-export interface User { 
-    email: string,
-    username?: string,
-    password: string
+export interface User {
+  _id: string;
+  email: string;
+  username: string;
+  accessToken?: string;
 }
 
+export interface UserResponse {
+  status: string;
+  data: User;
+}
 
-export interface AuthContextType { 
-    user: User | null;
-    token: string | null;
-    isAuthenticated: boolean;
-    saveSession: (token: string, user: User) => void
-    clearSession: () => void
+export interface AuthContextType {
+  user: User | null;
+  isAuthenticated: boolean;
+  saveSession: (user: User) => void;
+  clearSession: () => void;
+}
 
+export interface LoginBody {
+  email: string;
+  password: string;
+}
+
+export interface RegisterBody {
+  email: string;
+  username: string;
+  password: string;
 }
