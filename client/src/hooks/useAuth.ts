6346d8054daa019fe903c49f2cab11508fa2b0a8
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-15 16:49:47 +0200

    create useAuth hook

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,47 @@
+import { authAPI } from "../api/authApi";
+import { useAuthContext } from "../contexts/AuthContext"
+import type { User } from "../types/user.types";
+
+export const useLogin = () => {
+    const { saveSession } = useAuthContext();
+    
+    const loginHandler = async (email: string, password: string ): Promise<User> => {
+        try {
+            const user = await authAPI.login(email, password)
+            saveSession(user);
+            return user
+        } catch (error) {
+            if(error instanceof Error){
+                throw new Error(error.message)
+            }
+            throw new Error('Login Failed !')
+        }
+    }
+
+    return loginHandler
+
+}
+
+export const useRegister = () => {
+    const { saveSession } = useAuthContext();
+
+    const registerHandler = async (email: string, username: string, password: string): Promise<User> => {
+
+        try {
+            const user = await authAPI.register(email, username, password);
+            saveSession(user)
+            return user
+            
+        } catch (error) {
+            if (error instanceof Error){
+                throw new Error(error.message)
+            }
+            throw new Error('Register Failed !')
+        }
+
+
+
+    }
+
+    return registerHandler
+}
