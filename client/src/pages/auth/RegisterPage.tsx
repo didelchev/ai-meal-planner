05c86804256ccdd5c80c50e5c5e86fcfd748edd0
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-15 16:50:24 +0200

    update register page to use useRegister hook

--- client/src/pages/auth/RegisterPage.tsx
+++ client/src/pages/auth/RegisterPage.tsx
@@ -1,7 +1,37 @@
 import { Link } from 'react-router-dom';
 import './AuthPage.css';
+import { useState } from 'react';
+import type { RegisterBody } from '../../types/user.types';
+import { useRegister } from '../../hooks/useAuth';
 
 const RegisterView = () => {
+
+  const [authData, setAuthData ] = useState<RegisterBody>({
+    email: "",
+    username: "",
+    password: ""
+  });
+
+  const register = useRegister();
+
+
+  const registerHandler = async (e: React.SubmitEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      
+      try {
+        await register(authData.email, authData.username, authData.password)
+        console.log("Success")
+      } catch (error) {
+        console.log(error)
+      }
+
+  }
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAuthData({...authData, [e.target.name]: e.target.value})
+  }
+
+  
   return (
     <div className='auth-container'>
       <div className='auth-card'>
@@ -11,13 +41,16 @@ const RegisterView = () => {
           <p>Start your meal planning journey today</p>
         </div>
 
-        <div className='auth-form'>
+        <form className='auth-form' onSubmit={registerHandler}>
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
             <input
               id='username'
+              name='username'
               type='text'
               placeholder='johndoe'
+              value={authData.username}
+              onChange={changeHandler}
             />
           </div>
 
@@ -26,7 +59,10 @@ const RegisterView = () => {
             <input
               id='email'
               type='email'
+              name='email'
               placeholder='you@example.com'
+              value={authData.email}
+              onChange={changeHandler}
             />
           </div>
 
@@ -35,7 +71,10 @@ const RegisterView = () => {
             <input
               id='password'
               type='password'
+              name='password'
               placeholder='••••••••'
+              value={authData.password}
+              onChange={changeHandler}
             />
           </div>
 
@@ -43,13 +82,14 @@ const RegisterView = () => {
             <label htmlFor='confirmPassword'>Confirm Password</label>
             <input
               id='confirmPassword'
+              name='confirmPassword'
               type='password'
               placeholder='••••••••'
             />
           </div>
 
           <button className='auth-button'>Create Account</button>
-        </div>
+        </form>
 
         <div className='auth-footer'>
           <p>Already have an account? <Link to='/login'>Sign in</Link></p>
