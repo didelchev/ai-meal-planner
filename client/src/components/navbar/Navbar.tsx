dae8e2426699112804f1bb12a21608bb0ed08c69
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-16 19:34:47 +0200

    update navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
-import './Navbar.css';
 import { useAuthContext } from '../../contexts/AuthContext';
+import './Navbar.css';
+import { useLogout } from '../../hooks/useAuth';
 
 const Navbar = () => {
   const { isAuthenticated } = useAuthContext();
+  const logout = useLogout();
+  const isHomePage = location.pathname === "/"
 
   return (
     <nav className='navbar'>
@@ -14,14 +17,25 @@ const Navbar = () => {
         </Link>
 
         <div className='navbar-links'>
-          <a href='#how-it-works'>How it works</a>
-          <a href='#features'>Features</a>
-          <a href='#preview'>Preview</a>
+          {isHomePage && (
+          <>
+            <Link to="/#how-it-works">How it works</Link>
+            <a href='#features'>Features</a>
+            <a href='#stats'>Results</a>
+          </>
+          
+          )}
+          
         </div>
 
         <div className='navbar-auth'>
           {isAuthenticated ? (
-            <Link to='/dashboard' className='btn-primary'>Go to Dashboard</Link>
+            <>
+              <Link to='/dashboard' className='btn-ghost'>Dashboard</Link>
+              <Link to='/onboarding' className='btn-ghost'>Onboarding</Link>
+              <Link to='/profile' className='btn-ghost'>Profile</Link>
+              <button onClick={logout} className='btn-logout'>Logout</button>
+            </>
           ) : (
             <>
               <Link to='/login' className='btn-ghost'>Login</Link>
