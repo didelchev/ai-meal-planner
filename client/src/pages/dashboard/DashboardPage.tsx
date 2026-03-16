f158acbf8da3952bdad499bc2270d14077b57191
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-16 19:34:39 +0200

    create basic pages

---
+++ client/src/pages/dashboard/DashboardPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/navbar/Navbar"
+import "./DashboardPage.css"
+
+const DashboardPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div>DashboardPage</div>
+    </>
+  )
+}
+
+export default DashboardPage
