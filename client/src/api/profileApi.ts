7ec76aef3034e317a6321c96313830ca0e81fe78
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-17 13:53:09 +0200

    create profileApi

---
+++ client/src/api/profileApi.ts
@@ -0,0 +1,17 @@
+import type { ProfileBody, ProfileResponse } from "../types/user.types";
+import { API } from "../utils/fetcher";
+
+const PROFILE_API_ENDPOINT = 'http://localhost:5000/profile'
+
+
+export const profileAPI = {
+    async getUserProfile(): Promise<ProfileResponse>{
+        return API.get(PROFILE_API_ENDPOINT)
+    },
+
+
+    async createUserProfile(profileData: ProfileBody): Promise<ProfileResponse>{
+       return API.post(PROFILE_API_ENDPOINT, profileData) 
+    }
+
+};
