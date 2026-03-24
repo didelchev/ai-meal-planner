0aa0033dfc709602aaeb2900c3ee01ddb4035301
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-24 17:44:07 +0200

    renamed fetcher error message

--- client/src/utils/fetcher.ts
+++ client/src/utils/fetcher.ts
@@ -27,7 +27,7 @@ export const fetcher = async<T> (
 
     if(!response.ok){
       const error = await response.json();
-      throw new Error(`Response status: ${response.status}, ${error.message}`)
+      throw new Error(error.message)
     }
 
     const result = await response.json();
