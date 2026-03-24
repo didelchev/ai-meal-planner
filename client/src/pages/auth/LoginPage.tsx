26f8e17228c7ce44a925728fb10002835fff4e9e
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-24 17:47:22 +0200

    add loading and error components to login page

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -4,9 +4,11 @@ import { useLogin } from '../../hooks/useAuth';
 import { useForm } from '../../hooks/useForm';
 import type { LoginBody } from '../../types/user.types';
 import Navbar from '../../components/navbar/Navbar';
+import { ClipLoader } from 'react-spinners';
 
 const LoginPage = () => {
-  const login  = useLogin();
+
+  const {login, isLoading, error}  = useLogin();
   const initialValues = { email: "", password: ""}
   const navigate  = useNavigate();
 
@@ -14,10 +16,9 @@ const LoginPage = () => {
     try {
       await login(formData.email, formData.password)
       navigate("/")
-      console.log("success")
-    } catch (error) { 
-      console.log(`Error: ${error}`)
-    }
+    } catch (err) { 
+      console.error(error)
+    } 
   }
 
   const { formData, changeHandler, submitHandler } = useForm(initialValues, loginHandler);
@@ -25,51 +26,68 @@ const LoginPage = () => {
   
   return (
     <>
-    <Navbar/>
-    <div className='auth-container'>
-      <div className='auth-card'>
-        <div className='auth-header'>
-          <h1>Welcome back</h1>
-          <p>Sign in to your account to continue</p>
-        </div>
-
-        <form className='auth-form' onSubmit={submitHandler}>
-          <div className='form-group'>
-            <label htmlFor='email'>Email</label>
-            <input
-              id='email'
-              name='email'
-              type='email'
-              placeholder='you@example.com'
-              value={formData.email}
-              onChange={changeHandler}
-            />
+      <Navbar/>
+      <div className='auth-container'>
+        <div className='auth-card'>
+          <div className='auth-header'>
+            <h1>Welcome back</h1>
+            <p>Sign in to your account to continue</p>
           </div>
 
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              id='password'
-              name='password'
-              type='password'
-              placeholder='••••••••'
-              value={formData.password}
-              onChange={changeHandler}
-            />
+          <form className='auth-form' onSubmit={submitHandler}>
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                placeholder='you@example.com'
+                value={formData.email}
+                onChange={changeHandler}
+              />
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                placeholder='••••••••'
+                value={formData.password}
+                onChange={changeHandler}
+              />
+            </div>
+
+            <button className='auth-button'>
+              {isLoading ? (
+                <ClipLoader size={20} color="#eef2ff" />
+                ) : 
+                ('Sign in')}
+            </button>
+          </form>
+          {error && (
+            <div
+              style={{
+                color: "red",
+                border: "1px solid red",
+                padding: "10px",
+                marginTop: "10px",
+                textAlign: "center",
+              }}
+            >
+              Login Failed: {error}
+            </div>
+          )}
+
+          <div className='auth-footer'>
+            <p>Don't have an account? <Link to='/register'>Sign up</Link></p>
           </div>
-
-          <button className='auth-button'>Sign In</button>
-        </form>
-
-        <div className='auth-footer'>
-          <p>Don't have an account? <Link to='/register'>Sign up</Link></p>
         </div>
-
       </div>
-    </div>
     </>
-    
-  );
+  )
 };
 
-export default LoginPage;
+
+export default LoginPage
